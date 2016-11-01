e947016a35ab7cd227bc1e208b01c01f71db5b9b
郎啸宇 <langxiaoyu@autohome.com.cn>
2016-11-01 15:18:27 +0800

    修复bug

--- src/main.vue
+++ src/main.vue
@@ -4,6 +4,7 @@
  * @param { date } Date - 当前日期的Date实例. 默认为 new Date()
  * @param { className } String - 外部容器自定义类名. 默认为: calendar
  * @param { calendarTitle } String - 日历上方当前选中日期左侧文字. 默认为： 当前日期
+ * @param { isShow } Boolean - 控制日历显示隐藏
  * @param { startYear/endYear } String/Number - 可选的年份的范围. 默认为： 1901/2050
  * @param { change } CallBack - 日期变化触发回调. 传入参数: xxxx-xx-xx String类型
  * @author XiaoYu.Lang Email: webdeveloperfox@gmail.com
@@ -63,7 +64,7 @@ export default {
             type: String,
             default: '当前日期'
         },
-        isShwo: {
+        isShow: {
             type: Boolean,
             default: true
         },
